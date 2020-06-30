29806d5ab95531efafbaa18fc28533d7222a1dc0
obuseme <obusek@gmail.com>
2020-06-30 19:23:44 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Andrew Obusek on 6/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
